5513ed8978689dbf824a0dac07454052effb52d0
RishabhVerma098 <vermarishabh0987@gmail.com>
2023-02-09 23:59:08 +0530

    ci: import sort and added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import './index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App';
-import './index.scss';
+
+// Routes for the app.
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// tests/setup.ts
 import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
-import { expect, afterEach } from 'vitest';
+import { afterEach, expect } from 'vitest';
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
